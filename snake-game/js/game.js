84012b77b524fe21d1443a9aa53d04fbce9fbe77
Pritam Bohra <6594694+pritam1605@users.noreply.github.com>
2020-08-01 21:03:12 -0400

    Create game.js

---
+++ snake-game/js/game.js
@@ -0,0 +1,35 @@
+import {
+  draw as drawSnake,
+  SNAKE_SPEED_PER_SECOND,
+  update as updateSnake
+} from './snake.js';
+
+
+let lastRenderTime = 0;
+const gameBoard = document.querySelector('.game-board');
+
+function main(currentTime) {
+  const timeTakenToRender = (currentTime - lastRenderTime) / 1000;
+  window.requestAnimationFrame(main);
+
+  if (1 / SNAKE_SPEED_PER_SECOND > timeTakenToRender) {
+    return;
+  }
+
+  console.log(timeTakenToRender);
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+}
+
+function draw() {
+  gameBoard.innerHTML = '';
+  drawSnake(gameBoard);
+}

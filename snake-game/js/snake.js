8ef1060a09094a6bb645c77eb1ccc104e29f432c
Pritam Bohra <prit1605@gmail.com>
2020-08-03 22:16:51 -0400

    Almost finished the game

--- snake-game/js/snake.js
+++ snake-game/js/snake.js
@@ -1,50 +1,60 @@
 import {
   newPositionOfSnake
-} from './input.js';
+} from "./input.js";
 
-export const SNAKE_SPEED_PER_SECOND = 2;
+export const SNAKE_SPEED_PER_SECOND = 5;
+const SNAKE_GROWTH_SIZE = 1;
+const GRID_SIZE = 21;
+
 const snakeBody = [{
-    x: 10,
-    y: 11
-  },
-  {
-    x: 11,
-    y: 11
-  }, {
-    x: 12,
-    y: 11
-  },
-  {
-    x: 13,
-    y: 11
-  }, {
-    x: 14,
-    y: 11
-  }
-];
+  x: 10,
+  y: 11,
+}, ];
 
 export function draw(gameBoard) {
-
-  snakeBody.forEach(seg => {
-    const snakeEl = document.createElement('div');
+  snakeBody.forEach((seg) => {
+    const snakeEl = document.createElement("div");
     snakeEl.style.gridRowStart = seg.y;
     snakeEl.style.gridColumnStart = seg.x;
-    snakeEl.className = 'snake';
+    snakeEl.className = "snake";
 
     gameBoard.appendChild(snakeEl);
   });
-
 }
+
 export function update() {
+  if (isSnakeDead()) {
+    if (!confirm("Play Again?")) {
+      return;
+    }
+  }
   const position = newPositionOfSnake();
-  console.log(position);
 
   for (let i = snakeBody.length - 1; i > 0; i--) {
     snakeBody[i] = {
-      ...snakeBody[i - 1]
+      ...snakeBody[i - 1],
     };
   }
 
   snakeBody[0].x += position.x;
   snakeBody[0].y += position.y;
+}
+
+export function isOnSnake(position) {
+  return snakeBody.some(
+    (part) => position.x === part.x && position.y === part.y
+  );
+}
+
+export function growSnake() {
+
+  let count = 0;
+  while (count < SNAKE_GROWTH_SIZE) {
+    snakeBody.push(snakeBody[snakeBody.length - 1]);
+    count++;
+  }
+}
+
+function isSnakeDead() {
+  return snakeBody[0].y === 0 || snakeBody[0].y === 21 || snakeBody[0].x === 0 || snakeBody[0].x === 21;
 }
